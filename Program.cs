64abe5f1475d3f48bc1eb3cd1a6d6042eec02502
Joel Ringh <joel.ringh@gmail.com>
2025-09-23 11:50:23 +0200

    added borrower object and removed base Methods

--- Program.cs
+++ Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Librarian librarian = new Librarian();
+            User librarian = new Librarian();
+            User borrower = new Librarian();
 
             while (true)
             {
@@ -29,7 +30,7 @@
                         librarian.ShowMenu();
                         break;
                     case "2":
-                        LantagareMeny();
+                        borrower.ShowMenu();
                         break;
                     default:
                         Console.WriteLine("Felaktigt val, försök igen.");
@@ -40,63 +41,6 @@
 
                 Console.ReadLine();
             }
-        }
-
-        static void LantagareMeny()
-        {
-            while (true)
-            {
-                Console.Clear();
-                Console.WriteLine("Låntagare-meny:");
-                Console.WriteLine("1. Funktion från person 2");
-                Console.WriteLine("2. Funktion från person 3");
-                Console.WriteLine("3. Funktion från person 4");
-                Console.WriteLine("0. Tillbaka");
-                Console.Write("Ditt val: ");
-                string input = Console.ReadLine();
-
-                if (input == "0") break;
-
-                switch (input)
-                {
-                    case "1":
-                        Person2Metod();
-                        break;
-                    case "2":
-                        Person3Metod();
-                        break;
-                    case "3":
-                        Person4Metod();
-                        break;
-                    default:
-                        Console.WriteLine("Felaktigt val, försök igen.");
-                        Console.WriteLine("Tryck på valfri tangent för att fortsätta...");
-                        Console.ReadKey();
-                        break;
-                }
-            }
-        }
-
-        // Byt ut dessa mot riktiga metoder från person 2–4
-        static void Person2Metod()
-        {
-            Console.WriteLine("Person 2:s metod körs...");
-            Console.WriteLine("Tryck på valfri tangent för att fortsätta...");
-            Console.ReadKey();
-        }
-
-        static void Person3Metod()
-        {
-            Console.WriteLine("Person 3:s metod körs...");
-            Console.WriteLine("Tryck på valfri tangent för att fortsätta...");
-            Console.ReadKey();
-        }
-
-        static void Person4Metod()
-        {
-            Console.WriteLine("Person 4:s metod körs...");
-            Console.WriteLine("Tryck på valfri tangent för att fortsätta...");
-            Console.ReadKey();
         }
     }
 }
