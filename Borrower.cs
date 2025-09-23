653421f03b8958ab17517aae23456e4c5d5dfc2d
Joel Ringh <joel.ringh@gmail.com>
2025-09-23 11:48:49 +0200

    Added menu for Borrower class

--- Borrower.cs
+++ Borrower.cs
@@ -8,14 +8,16 @@ namespace Bibliotekssytem
 {
     public class Borrower : User
     {
+        
         public override void ShowMenu()
         {
-            Console.WriteLine("Librarian Menu");
-            Console.WriteLine("1. Add Book");
-            Console.WriteLine("2. Remove Book");
-            Console.WriteLine("3. Search Book");
-            Console.WriteLine("4. View All Books");
+            Console.WriteLine("Borrower Menu");
+            Console.WriteLine("1. Search Book");
+            Console.WriteLine("2. View All Books");
+            Console.WriteLine("3. Borrow Book");
+            Console.WriteLine("4. Return Book");
             Console.WriteLine("5. Exit");
         } // kraftigt genererad med AI
+
     }
 }
