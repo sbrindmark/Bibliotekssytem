750c458cf336637fca37111172f2de671948cc44
Joel Ringh <joel.ringh@gmail.com>
2025-09-24 17:35:08 +0200

    Removed uneccessary get setters and made books fields private

--- Books.cs
+++ Books.cs
@@ -9,9 +9,9 @@ namespace Bibliotekssytem
 {
     public class Books : ISearchable
     {
-        public string ISBN { get; set; }
-        public string Title { get; set; }
-        public string Author { get; set; }
+        string ISBN;
+        string Title;
+        string Author;
 
         public Books(string title, string author, string isbn)
         {
