08f245f37801237d250346e4ee2238718675f567
Joel Ringh <joel.ringh@gmail.com>
2025-09-25 10:08:20 +0200

    Changed borrower meny to not get stuck in a loop

--- Borrower.cs
+++ Borrower.cs
@@ -13,37 +13,38 @@ namespace Bibliotekssytem
         {
             bool running = true;
             while (running)
-
-            Console.WriteLine("Borrower Menu");
-            Console.WriteLine("1. Search Book");
-            Console.WriteLine("2. View All Books");
-            Console.WriteLine("3. Borrow Book");
-            Console.WriteLine("4. Return Book");
-            Console.WriteLine("5. Exit");
-            // kraftigt genererad med AI
-            var input = Console.ReadLine();
-
-            switch (input)
             {
-                case "1":
-                    // Sök efter bok
-                    break;
-                case "2":
-                    //ListBooks(); - ###hittar metoden då den är definierad i en annan klass.####
-                    break;
-                case "3":
-                    //Låna bok
-                    break;
-                case "4":
-                    //Lämna tillbaka bok
-                    break;
-                case "5":
-                    running = false;
-                    break;
-                default:
-                    Console.WriteLine("Ogiltigt val.");
-                    break;
+                Console.WriteLine("Borrower Menu");
+                Console.WriteLine("1. Search Book");
+                Console.WriteLine("2. View All Books");
+                Console.WriteLine("3. Borrow Book");
+                Console.WriteLine("4. Return Book");
+                Console.WriteLine("5. Exit");
+                // kraftigt genererad med AI
+                var input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "1":
+                        // Sök efter bok
+                        break;
+                    case "2":
+                        //ListBooks(); - ###hittar metoden då den är definierad i en annan klass.####
+                        break;
+                    case "3":
+                        //Låna bok
+                        break;
+                    case "4":
+                        //Lämna tillbaka bok
+                        break;
+                    case "5":
+                        break;
+                    default:
+                        Console.WriteLine("Ogiltigt val.");
+                        break;
+                }
             }
+
         } 
 
         public List<Books> borrowedBooks = new List<Books>();
